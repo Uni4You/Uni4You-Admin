1c18dca87e5fef2f46596a3c430038761a33dbd9
Timo Staudinger <staudinger.timo@gmail.com>
2014-06-16 22:21:01 +0200

    User Management implemented.

--- FacultyInfo_Admin/src/FacultyInfo/UserBundle/Controller/OverviewController.php
+++ FacultyInfo_Admin/src/FacultyInfo/UserBundle/Controller/OverviewController.php
@@ -37,4 +37,35 @@ class OverviewController extends Controller {
 		return $this -> redirect($this -> generateUrl('facultyinfo_user_overview'));
 	}
 
+	public function resetAction($userId) {
+		$em = $this -> container -> get('doctrine') -> getManager();
+		$user = $em -> getRepository('FacultyInfoUserBundle:User') -> find($userId);
+
+		if (!$user) {
+			$this -> get('session') -> getFlashBag() -> add('error', $this -> get('translator') -> trans('userManagement.error.invalidUserId'));
+			return $this -> redirect($this -> generateUrl('facultyinfo_user_overview'));
+		}
+
+		$pwd = $this -> generatePassword();
+		$currentUser = $this -> get('security.context') -> getToken() -> getUser();
+		$encoder = $this -> container -> get('facultyinfo_user_custom_encoder');
+		$encodedPwd = $encoder -> encodePassword($pwd, $currentUser -> getSalt());
+		$user -> setPassword($encodedPwd);
+
+		$em -> flush();
+
+		$this -> get('session') -> getFlashBag() -> add('success', $this -> get('translator') -> trans('userManagement.passwordReset.successful', array('%name%' => $user -> getName(), '%pwd%' => $pwd)));
+		return $this -> redirect($this -> generateUrl('facultyinfo_user_overview'));
+	}
+
+	private function generatePassword() {
+		$length = 10;
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, strlen($characters) - 1)];
+		}
+		return $randomString;
+	}
+
 }
--- FacultyInfo_Admin/src/FacultyInfo/UserBundle/Form/Type/PasswordType.php
+++ FacultyInfo_Admin/src/FacultyInfo/UserBundle/Form/Type/PasswordType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 class PasswordType extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		$builder -> add('password', 'repeated', array('first_name' => 'pwd', 'second_name' => 'pwd_repeat', 'invalid_message' => 'userManagement.update.passwordsDontMatch' ,'first_options' => array('label' => 'userManagement.labels.firstPassword'), 'second_options' => array('label' => 'userManagement.labels.secondPassword')));
+		$builder -> add('password', 'repeated', array('type' => 'password', 'first_name' => 'pwd', 'second_name' => 'pwd_repeat', 'invalid_message' => 'userManagement.update.passwordsDontMatch' ,'first_options' => array('label' => 'userManagement.labels.firstPassword'), 'second_options' => array('label' => 'userManagement.labels.secondPassword')));
 		$builder -> add('save', 'submit', array('label' => 'userManagement.update.savePassword'));
 	}
 
